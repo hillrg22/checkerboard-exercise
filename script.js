8da29c93902a221c79f7c148837c8a7fab06e179
hillrg22 <hillrg22@gmail.com>
2018-09-11 15:55:44 -0600

    adjusted width and height properties to get checkerboard to fill entire page.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
 var body = document.querySelector('body')
 
-for (let i=0; i < 24; i++){
+for (let i=0; i < 60; i++){
 var div = document.createElement('div')
 var div2 = document.createElement('div')
 body.appendChild(div)
 body.appendChild(div2)
 div.setAttribute('class', 'red')
 div2.setAttribute('class', 'black')
-div.style.width = '11.1vw'
-div.style.height = '11.1vw'
+div.style.width = '11.1%'
+div.style.height = '11.1%'
 div.style.float = 'left'
-div.style.paddingBottom = '11.1vw'
-div.style.backgroundImage = 'linear-gradient(#000,#000), linear-gradient(red, red)'
+div.style.paddingBottom = '11.1%'
+div.style.backgroundImage = 'linear-gradient(black,black), linear-gradient(red, red)'
 div.style.backgroundClip = 'content-box, padding-box'
-div2.style.width = '11.1vw'
-div2.style.height = '11.1vw'
+div2.style.width = '11.1%'
+div2.style.height = '11.1%'
 div2.style.float = 'left'
-div2.style.paddingBottom = '11.1vw'
+div2.style.paddingBottom = '11.1%'
 div2.style.backgroundImage = 'linear-gradient(red, red), linear-gradient(black, black)'
 div2.style.backgroundClip = 'content-box, padding-box'
 
