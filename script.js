1a7c85847353dd7d090982f719d2c6501976673d
hillrg22 <hillrg22@gmail.com>
2018-09-11 15:47:35 -0600

    adjusted divs to include a gradient and different colored padding

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-for(let i=0; i < 50; i++){
-let div = document.createElement('div')
-let div2 = document.createElement('div')
-let body = document.querySelector('body')
+
+var body = document.querySelector('body')
+
+for (let i=0; i < 24; i++){
+var div = document.createElement('div')
+var div2 = document.createElement('div')
 body.appendChild(div)
 body.appendChild(div2)
 div.setAttribute('class', 'red')
 div2.setAttribute('class', 'black')
-div.style.backgroundColor = 'red'
-div2.style.backgroundColor = 'black'
-div.style.float = 'left'
-div2.style.float = 'left'
 div.style.width = '11.1vw'
+div.style.height = '11.1vw'
+div.style.float = 'left'
+div.style.paddingBottom = '11.1vw'
+div.style.backgroundImage = 'linear-gradient(#000,#000), linear-gradient(red, red)'
+div.style.backgroundClip = 'content-box, padding-box'
 div2.style.width = '11.1vw'
-div.style.height = '11.1vh'
-div2.style.height = '11.1vh'
-div.style.paddingBottom = '11.1vh'
-div2.style.paddingBottom = '11.1vh'
+div2.style.height = '11.1vw'
+div2.style.float = 'left'
+div2.style.paddingBottom = '11.1vw'
+div2.style.backgroundImage = 'linear-gradient(red, red), linear-gradient(black, black)'
+div2.style.backgroundClip = 'content-box, padding-box'
+
 }
