2ff0e8c3c26bc5823f1a47bfe3ba905b0e8af81f
hillrg22 <hillrg22@gmail.com>
2018-09-11 14:14:27 -0600

    used js file to create div elements in body for black squares and red squares. Began styling div sizes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+for(let i=0; i < 50; i++){
+let div = document.createElement('div')
+let div2 = document.createElement('div')
+let body = document.querySelector('body')
+body.appendChild(div)
+body.appendChild(div2)
+div.setAttribute('class', 'red')
+div2.setAttribute('class', 'black')
+div.style.backgroundColor = 'red'
+div2.style.backgroundColor = 'black'
+div.style.float = 'left'
+div2.style.float = 'left'
+div.style.width = '11.1vw'
+div2.style.width = '11.1vw'
+div.style.height = '11.1vh'
+div2.style.height = '11.1vh'
+div.style.paddingBottom = '11.1vh'
+div2.style.paddingBottom = '11.1vh'
+}
