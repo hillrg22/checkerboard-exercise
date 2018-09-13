2e8c07883511e21cdc5ee4cdb19b7cc2253c56dc
hillrg22 <hillrg22@gmail.com>
2018-09-13 13:04:39 -0600

    incorporated randomColors function into checkerboard for loop

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 
 var body = document.querySelector('body')
+body.style.width = '100%'
+body.style.height = '100%'
+
 
 const rgbConst = 255
 function randomColors(){
-return 'rgb(' + Math.round(rgbConst*Math.random())+ ', ' + Math.round(rgbConst*Math.random()) + ', ' + Math.round(rgbConst*Math.random())+ ')'
+var rand = 'rgb(' + Math.round(rgbConst*Math.random())+ ', ' + Math.round(rgbConst*Math.random()) + ', ' + Math.round(rgbConst*Math.random())+ ')'
+return `${rand},${rand}`
 }
 
-function checkerBoard(){
-  for (let i=0; i < 60; i++){
-    var div = document.createElement('div')
-    var div2 = document.createElement('div')
-    body.appendChild(div)
-    body.appendChild(div2)
-    div.setAttribute('class', 'red')
-    div2.setAttribute('class', 'black')
-    div.style.width = '11.1%'
-    div.style.height = '11.1%'
-    div.style.float = 'left'
-    div.style.paddingBottom = '11.1%'
-    div.style.backgroundImage = 'linear-gradient(black,black), linear-gradient(red, red)'
-    div.style.backgroundClip = 'content-box, padding-box'
-    div2.style.width = '11.1%'
-    div2.style.height = '11.1%'
-    div2.style.float = 'left'
-    div2.style.paddingBottom = '11.1%'
-    div2.style.backgroundImage = 'linear-gradient(red, red), linear-gradient(black, black)'
-    div2.style.backgroundClip = 'content-box, padding-box'
 
-  }
+
+for (let i=0; i < 200; i++){
+var div = document.createElement('div')
+var div2 = document.createElement('div')
+body.appendChild(div)
+body.appendChild(div2)
+div.setAttribute('class', 'red')
+div2.setAttribute('class', 'black')
+div.style.width = "11.1%"
+div.style.height = "11.1%"
+div.style.float = 'left'
+div.style.paddingBottom = "11.1%"
+div.style.backgroundImage = `linear-gradient(${randomColors()}), linear-gradient(${randomColors()})`
+div.style.backgroundClip = 'content-box, padding-box'
+div2.style.width = "11.1%"
+div2.style.height = "11.1%"
+div2.style.float = 'left'
+div2.style.paddingBottom = "11.1%"
+div2.style.backgroundImage = `linear-gradient(${randomColors()}), linear-gradient(${randomColors()})`
+div2.style.backgroundClip = 'content-box, padding-box'
+
+
 }
